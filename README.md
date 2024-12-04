822b5968000a5297c5c945720ccc039814dcd5f5
Albert Mpepo <albertmpepo@gmail.com>
2024-12-04 16:08:36 +0200

    Completed Project

---
+++ README.md
@@ -0,0 +1,29 @@
+# Basketball Scoreboard App
+This project is a simple basketball scoreboard application built using React. It allows users to keep track of the score for two teams, increment scores by various amounts (1, 2, or 3 points), and reset the game.
+
+## Features
+- Displays scores for two teams (Home and Guest).
+- Allows incrementing scores by 1, 2, or 3 points for each team.
+- Includes a reset button to start a new game.
+- Simple and intuitive user interface.
+
+## Getting Started
+These instructions will guide you in setting up a local copy of the project for development and testing purposes.
+
+### Prerequisites
+- You should have Node.js and npm (or yarn) installed on your system.
+
+### Installation
+1. Clone the repository: Use your preferred method to clone the project repository to your local machine.
+2. Install dependencies: Navigate to the project directory in your terminal and run npm install or yarn install to install the required dependencies.
+
+### Running the App
+1. Start the development server: Run npm start or yarn start to start the development server. This will open the app in your default browser.
+2. Use the app: You can now use the scoreboard to track the game. Click the "+1", "+2", and "+3" buttons to increment the scores for each team. Click the "Reset" button to start a new game.
+
+## Built With
+- React - The web framework used
+- Tailwind CSS - A utility-first CSS framework
+
+## Acknowledgments
+We would like to thank [Scrimba Team](https://www.figma.com/design/YC48MCx4frBFtYoz6rNJE6/Basketball-Scoreboard?node-id=0-1&node-type=canvas&t=hq9VsKo491vHHnYz-0) for the amazing design work used in this project. You can explore the design further in the [Figma file](https://www.figma.com/design/YC48MCx4frBFtYoz6rNJE6/Basketball-Scoreboard?node-id=0-1&node-type=canvas&t=hq9VsKo491vHHnYz-0).
---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [home, setHome] = useState(0)
+  const [guest, setGuest] = useState(0)
+
+  const handleClickHome = (point: number) => setHome(home + point)
+  const handleClickGuest = (point: number) => setGuest(guest + point)
+
+
+  return (
+    <>
+      <div className="relative flex min-h-screen flex-row justify-center gap-24 overflow-hidden bg-blue-950 py-6 sm:py-12">
+        <div className="flex flex-col">
+          <h1 className="font-verdana mx-auto font-bold tracking-widest text-white text-2xl">HOME</h1>
+          <div className="font-cursedtimer text-8xl rounded bg-black px-24 py-16 text-red-500">{home}</div>
+          <div className="font-cursedtimer my-7 flex justify-between">
+            <button 
+              className="rounded-md border border-current p-4 text-slate-400"
+              onClick={() => handleClickHome(1)}
+            >
+                + 1
+            </button>
+            <button className="rounded-md border border-current p-4 text-slate-400"
+              onClick={() => handleClickHome(2)}
+            >
+              + 2
+            </button>
+            <button className="rounded-md border border-current p-4 text-slate-400"
+              onClick={() =>handleClickHome(3)}
+            >
+              + 3
+            </button>
+          </div>
+          <button className="rounded-md border border-current p-4 text-slate-400" onClick={() => {setHome(0);}}>Reset</button>
+        </div>
+        <div className="flex flex-col">
+          <h1 className="font-verdana mx-auto font-bold tracking-widest text-white text-2xl">GUEST</h1>
+          <div className="font-cursedtimer text-8xl rounded bg-black px-24 py-16 text-red-500">{guest}</div>
+          <div className="font-cursedtimer my-7 flex justify-between">
+            <button className="rounded-md border border-current p-4 text-slate-400"
+              onClick={() => handleClickGuest(1)}
+            >
+              + 1
+            </button>
+            <button className="rounded-md border border-current p-4 text-slate-400"
+              onClick={() => handleClickGuest(2)}
+            >
+              + 2
+            </button>
+            <button className="rounded-md border border-current p-4 text-slate-400"
+              onClick={() => handleClickGuest(3)}
+            >
+              + 3
+            </button>
+          </div>
+          <button className="rounded-md border border-current p-4 text-slate-400" onClick={() => {setGuest(0);}}>Reset</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
